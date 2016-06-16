285a6c87584045662696f8068f8eca22f6b9aeca
Katherine Leipper <kleipper@gmail.com>
2016-06-15 21:27:26 -0700

    Simple test to check for ForStatement

--- test/code-box.js
+++ test/code-box.js
@@ -7,8 +7,47 @@ var chai = require('chai');
 var expect = chai.expect;
 var should = chai.should();
 
-describe('Compare Numbers', function() {
-	it('1 should equal 1', function() {
-	  expect(1).to.equal(1);
+var input = "var foo = 5;"
+
+describe('CodeBox', function() {
+
+	function traverse(node, func) {
+	    func(node);//1
+	    for (var key in node) { //2
+	        if (node.hasOwnProperty(key)) { //3
+	            var child = node[key];
+	            if (typeof child === 'object' && child !== null) { //4
+	                if (Array.isArray(child)) {
+	                    child.forEach(function(node) { //5
+	                        traverse(node, func);
+	                    });
+	                } else {
+	                    traverse(child, func); //6
+	                }
+	            }
+	        }
+	    }
+	}
+
+	function analyzeCode(code, type) {
+	    var ast = esprima.parse(code);
+	    var var_types = [];
+	    traverse(ast, function(node) {
+        //console.log(node.type);
+        var_types.push(node.type);
+	    });
+	    if (var_types.includes(type)) {
+	    	return true;
+	    } else {
+	    	return false;
+	    }
+	}
+
+	var types = ['ForStatement', 'VariableDeclaration'];
+
+	it('should include a ForStatement', function() {
+		expect(analyzeCode(input, 'ForStatement')).to.equal(true);
 	});
+
 });
+
